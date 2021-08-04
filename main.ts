81233ba4c9a6a69dc05a4834c01bfd622b54fd96
unknown658 <87977019+unknown658@users.noreply.github.com>
2021-08-04 10:32:09 +0100

    Added test block to display from typescript

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
+/**
+ * Test extension for checking offline build
+ */
 
+//% weight=100 color=#aef354 icon="\uf0c2" block="Test 1"
+namespace test_numero_uno {
+    /** 
+     * Test function to display some text.
+     */
+    //% weight=100 blockGap=8
+    //% blockId="test_numero_uno_show_text_ts" block="display some text with ts"
+    export function showText_ts(): void {
+        basic.showString("Hello from TS!")
+    }
+}
